8e68ab33479957ea6bbaf66e9ccfcddc4d54b1f3
user189869 <user189869@74-80-245-35.ic2net.net>
2021-05-13 12:45:07 -0700

    Full screen y correccion titulo

---
+++ QueTeInteresaAprenderViewController.swift
@@ -0,0 +1,31 @@
+//
+//  QueTeInteresaAprenderViewController.swift
+//  APPa
+//
+//  Created by user189869 on 5/13/21.
+//
+
+import UIKit
+import AVFoundation
+
+class QueTeInteresaAprenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "¿Qué te interesa aprender?";
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
