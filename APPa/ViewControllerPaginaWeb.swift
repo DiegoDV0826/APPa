0bf14ab6b88a34cd36a2475b722c57d66e6d160f
cmm23 <A00825117@itesm.mx>
2021-05-16 20:11:11 -0500

    Drag&Drop entre dos collection views

--- APPa/ViewControllerPaginaWeb.swift
+++ APPa/ViewControllerPaginaWeb.swift
@@ -8,10 +8,12 @@
 import UIKit
 import AVFoundation
 
-class ViewControllerPaginaWeb: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout  {
+class ViewControllerPaginaWeb: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDragDelegate, UICollectionViewDropDelegate  {
+    
 
     var audioPlayer: AVAudioPlayer?
-    var info: [String] = ["Nombre del autor", "Lugar de publicacion", "Editorial", "Año", "Titulo", "Edicion"]
+    var info: [String] = ["Renteria Salazar, P.", "Bogotá, Colombia", "Renovacion Urbana", "2006", "El comienzo de la renovación", "Ed."]
+    var info2: [String] = ["Nombre del autor", "Lugar de publicacion", "Editorial", "Año", "Titulo", "Edicion"]
     
     @IBOutlet weak var firstCollectionView: UICollectionView!
     @IBOutlet weak var secondCollectionView: UICollectionView!
@@ -19,10 +21,16 @@ class ViewControllerPaginaWeb: UIViewController, UICollectionViewDelegate, UICol
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.firstCollectionView.dragDelegate = self
+        self.firstCollectionView.dropDelegate = self
+        self.firstCollectionView.dragInteractionEnabled = true
+        
+        self.secondCollectionView.dragDelegate = self
+        self.secondCollectionView.dropDelegate = self
+        self.secondCollectionView.dragInteractionEnabled = true
+        self.secondCollectionView.reorderingCadence = .fast
 
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture(_:)))
-        gesture.minimumPressDuration = 0.0025
-        firstCollectionView?.addGestureRecognizer(gesture)
     }
     
 
@@ -59,61 +67,189 @@ class ViewControllerPaginaWeb: UIViewController, UICollectionViewDelegate, UICol
             }
     }
     
-    @objc func handleLongPressGesture(_ gesture: UILongPressGestureRecognizer){
-        guard let collectionView = firstCollectionView else {
-            return
-        }
-        switch gesture.state {
-        case .began:
-            guard let targetIndexPath = collectionView.indexPathForItem(at: gesture.location(in: collectionView)) else {
-                return
-            }
-            collectionView.beginInteractiveMovementForItem(at: targetIndexPath)
-        case .changed:
-            collectionView.updateInteractiveMovementTargetPosition(gesture.location(in: collectionView))
-        case .ended:
-            collectionView.endInteractiveMovement()
-        default:
-            collectionView.cancelInteractiveMovement()
-        }
-    }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 143, height: 45)
+        
+        if collectionView == firstCollectionView {
+            let size = info[indexPath.row].count
+            let wid = size * 8 + 12
+            return CGSize(width: wid, height: 30)
+        }
+        else {
+            let size = info2[indexPath.row].count
+            let wid = size * 8 + 12
+            return CGSize(width: wid, height: 30)
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return info.count
+        if collectionView == firstCollectionView {
+            return info.count
+        }
+        return info2.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        var cell = UICollectionViewCell()
-        if let infoCell = firstCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CollectionViewCell {
-            infoCell.configure(with: info[indexPath.row])
-            cell = infoCell
+        if collectionView == firstCollectionView{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+            cell.configure(with: info[indexPath.row])
             cell.backgroundColor = UIColor.white
             cell.layer.borderColor = UIColor.gray.cgColor
             cell.layer.borderWidth = 1
             cell.layer.cornerRadius = 5
+            return cell
         }
-        if collectionView == secondCollectionView {
+        else {
             let cell2 = secondCollectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! CollectionViewCell2
-            cell2.backgroundColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1)
-            cell2.configure(with: info[indexPath.row])
+            /*cell2.backgroundColor = UIColor(red: 205/255, green: 205/255, blue: 205/255, alpha: 1)
+            cell2.configure(with: info2[indexPath.row])*/
+            cell2.backgroundColor = UIColor.white
+            cell2.layer.borderColor = UIColor.gray.cgColor
+            cell2.layer.borderWidth = 1
+            cell2.layer.cornerRadius = 5
+            cell2.configure(with: info2[indexPath.row])
             return cell2
         }
-        return cell
-    }
-
-    //Re-order functions
-    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
-        return true
     }
     
-    //Swap positions in the array
-    func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let item = info.remove(at: sourceIndexPath.row)
-        info.insert(item, at: destinationIndexPath.row)
+    //Re order items whithin the same collection view
+    private func reorderItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView)
+    {
+        let items = coordinator.items
+        if items.count == 1, let item = items.first, let sourceIndexPath = item.sourceIndexPath
+        {
+            var dIndexPath = destinationIndexPath
+            if dIndexPath.row >= collectionView.numberOfItems(inSection: 0)
+            {
+                dIndexPath.row = collectionView.numberOfItems(inSection: 0) - 1
+            }
+            collectionView.performBatchUpdates({
+                if collectionView === self.secondCollectionView
+                {
+                    self.info2.remove(at: sourceIndexPath.row)
+                    self.info2.insert(item.dragItem.localObject as! String, at: dIndexPath.row)
+                }
+                else
+                {
+                    self.info.remove(at: sourceIndexPath.row)
+                    self.info.insert(item.dragItem.localObject as! String, at: dIndexPath.row)
+                }
+                collectionView.deleteItems(at: [sourceIndexPath])
+                collectionView.insertItems(at: [dIndexPath])
+            })
+            coordinator.drop(items.first!.dragItem, toItemAt: dIndexPath)
+        }
     }
+    
+    //Copy cell from collection 1 in collection 2
+    private func copyItems(coordinator: UICollectionViewDropCoordinator, destinationIndexPath: IndexPath, collectionView: UICollectionView)
+    {
+        collectionView.performBatchUpdates({
+            var indexPaths = [IndexPath]()
+            for (index, item) in coordinator.items.enumerated()
+            {
+                let indexPath = IndexPath(row: destinationIndexPath.row + index, section: destinationIndexPath.section)
+                if collectionView === self.secondCollectionView
+                {
+                    self.info2.insert(item.dragItem.localObject as! String, at: indexPath.row)
+                }
+                else
+                {
+                    self.info.insert(item.dragItem.localObject as! String, at: indexPath.row)
+                }
+                indexPaths.append(indexPath)
+            }
+            collectionView.insertItems(at: indexPaths)
+        })
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem]
+    {
+        let item = collectionView == firstCollectionView ? self.info[indexPath.row] : self.info2[indexPath.row]
+        let itemProvider = NSItemProvider(object: item as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = item
+        return [dragItem]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, itemsForAddingTo session: UIDragSession, at indexPath: IndexPath, point: CGPoint) -> [UIDragItem]
+    {
+        let item = collectionView == firstCollectionView ? self.info[indexPath.row] : self.info2[indexPath.row]
+        let itemProvider = NSItemProvider(object: item as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = item
+        return [dragItem]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dragPreviewParametersForItemAt indexPath: IndexPath) -> UIDragPreviewParameters?
+    {
+        if collectionView == firstCollectionView
+        {
+            let previewParameters = UIDragPreviewParameters()
+            previewParameters.visiblePath = UIBezierPath(rect: CGRect(x: 25, y: 25, width: 120, height: 120))
+            return previewParameters
+        }
+        return nil
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, canHandle session: UIDropSession) -> Bool
+    {
+        return session.canLoadObjects(ofClass: NSString.self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, dropSessionDidUpdate session: UIDropSession, withDestinationIndexPath destinationIndexPath: IndexPath?) -> UICollectionViewDropProposal
+    {
+        if collectionView === self.firstCollectionView
+        {
+            if collectionView.hasActiveDrag
+            {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+            }
+            else
+            {
+                return UICollectionViewDropProposal(operation: .forbidden)
+            }
+        }
+        else
+        {
+            if collectionView.hasActiveDrag
+            {
+                return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+            }
+            else
+            {
+                return UICollectionViewDropProposal(operation: .copy, intent: .insertAtDestinationIndexPath)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator)
+    {
+        let destinationIndexPath: IndexPath
+        if let indexPath = coordinator.destinationIndexPath
+        {
+            destinationIndexPath = indexPath
+        }
+        else
+        {
+            // Get last index path of table view.
+            let section = collectionView.numberOfSections - 1
+            let row = collectionView.numberOfItems(inSection: section)
+            destinationIndexPath = IndexPath(row: row, section: section)
+        }
+        
+        switch coordinator.proposal.operation
+        {
+        case .move:
+            self.reorderItems(coordinator: coordinator, destinationIndexPath:destinationIndexPath, collectionView: collectionView)
+            break
+            
+        case .copy:
+            self.copyItems(coordinator: coordinator, destinationIndexPath: destinationIndexPath, collectionView: collectionView)
+            
+        default:
+            return
+        }
+    }
+    
     
 }
