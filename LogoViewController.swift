c2a13d555c44b005e7d42319334e1830c7e3ba50
Diego del Valle <diegodelvalle2000@gmail.com>
2021-05-27 21:40:52 -0700

    Commit chido
    cruz azul

--- LogoViewController.swift
+++ LogoViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class LogoViewController: UIViewController {
 
+    @IBOutlet var vFondo: UIView!
+    @IBOutlet weak var imgLogo: UIImageView!
     override func viewDidLoad() {
            super.viewDidLoad()
            // Do any additional setup after loading the view, typically from a nib.
+        let defaults = UserDefaults.standard
+        let theme = defaults.bool(forKey: "fondo")
+        if theme {
+            imgLogo.image = UIImage(named: "2")
+            vFondo.backgroundColor = UIColor.init(displayP3Red: 42/255, green: 69/255, blue: 96/255, alpha: 255/255)
+        }else{
+            imgLogo.image = UIImage(named: "APPa logo")
+            vFondo.backgroundColor = UIColor.white
+        }
         let timer = Timer.scheduledTimer(timeInterval: 1.5, target: self, selector: #selector(timeToMoveOn), userInfo: nil, repeats: false)
+        
        }
 
        @objc func timeToMoveOn() {
