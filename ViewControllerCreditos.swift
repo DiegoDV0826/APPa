c175121706f40b192fc97ad0be3f6dee84f14385
Diego del Valle <diegodelvalle2000@gmail.com>
2021-05-30 21:55:18 -0700

    Ultimos Cambios

---
+++ ViewControllerCreditos.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerCreditos.swift
+//  APPa
+//
+//  Created by user189690 on 5/30/21.
+//
+
+import UIKit
+
+class ViewControllerCreditos: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask { return UIInterfaceOrientationMask.landscape
+    }
+    
+    override var shouldAutorotate: Bool {
+            return false
+    }
+
+}
